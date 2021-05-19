7f5111f447cbf18ca121ac363abd48d588fe3582
camposCode <info@josecampos.io>
2021-05-19 18:43:52 +0200

    router done

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,36 @@ import React from 'react';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
+import {
+  BrowserRouter as Router,
+    Redirect,
+    Route,
+  Switch,
+  useLocation
+} from "react-router-dom";
+
 
 const App = () => {
+
   return (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path = '/' component = { Main } />
+                <Route exact path = '/gallery' component = { Gallery }/>
+                <Redirect to = '/' />
+            </Switch>
+
+            <Footer />
+        </div>
+    </Router>
+)
+  /* return (
     <div>
       <Main />
       <Footer />
     </div>
-  )
+  ) */
 }
 
 export default App 
